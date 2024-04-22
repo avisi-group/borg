0f0790e318c4ca42d8065a0ba598476f8f7b7c49
Ferdia McKeogh <ferdia@mckeogh.tech>
2024-04-22 17:34:47 +0100

    Add additional assertions to `standalone` with linux kernel

--- brig/standalone/src/main.rs
+++ brig/standalone/src/main.rs
@@ -8,29 +8,35 @@ use {
 fn main() {
     let cli = Cli::parse();
 
-    unsafe {
-        rustix::mm::mmap_anonymous(
-            0x10_000 as *mut _,
-            12 * 1024 * 1024 * 1024,
-            ProtFlags::READ | ProtFlags::WRITE,
-            MapFlags::FIXED | MapFlags::PRIVATE,
-        )
-    }
-    .unwrap();
+    let image = fs::read(cli.path).unwrap();
 
-    let text_section = fs::read(cli.path).unwrap();
+    let header = unsafe { &*(image.as_ptr() as *const Arm64KernelHeader) };
+    if header.magic == ARM64_MAGIC {
+        assert_eq!(0, header.text_offset);
+    }
 
     let mut state = State::init();
+    state.write_register(REG_U_PC, 0);
 
     let mut instructions_retired = 0u64;
 
     let mut last_instrs = 0;
     let mut last_time = Instant::now();
 
+    unsafe {
+        rustix::mm::mmap_anonymous(
+            0x10_000 as *mut _,
+            64 * 1024 * 1024,
+            ProtFlags::READ | ProtFlags::WRITE,
+            MapFlags::FIXED | MapFlags::PRIVATE,
+        )
+    }
+    .unwrap();
+
     loop {
         let pc = state.read_register::<u64>(REG_U_PC);
 
-        let insn_data = unsafe { *(text_section.as_ptr().offset(pc as isize) as *const u32) };
+        let insn_data = unsafe { *(image.as_ptr().offset(pc as isize) as *const u32) };
 
         let res = if cli.verbose {
             decode_execute(insn_data, &mut state, &PrintlnTracer)
@@ -104,4 +110,21 @@ impl Tracer for PrintlnTracer {
     fn write_register<T: Debug>(&self, offset: usize, value: T) {
         println!("    R[{offset:x}] <- {value:?}");
     }
+}
+
+const ARM64_MAGIC: u32 = 0x644d5241;
+
+#[derive(Debug)]
+#[repr(C)]
+struct Arm64KernelHeader {
+    code0: u32,
+    code1: u32,
+    text_offset: u64,
+    image_size: u64,
+    flags: u64,
+    res2: u64,
+    res3: u64,
+    res4: u64,
+    magic: u32,
+    res5: u32,
 }
