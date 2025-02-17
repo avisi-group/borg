637ab2807f3e12f8a7825183fec5c41d0063d911
Ferdia McKeogh <ferdia@mckeogh.tech>
2025-02-17 10:49:39 +0000

    Fix silent errors on unknown tests in config

--- brig/kernel/src/tests.rs
+++ brig/kernel/src/tests.rs
@@ -3,18 +3,48 @@ use {alloc::vec::Vec, common::TestConfig, linkme::distributed_slice, proc_macro_
 #[distributed_slice]
 pub static TESTS: [(&str, fn())];
 
+// could be simpler, but I want a panic if we ever pass in an unknown test
+// instead of a silent fail
 pub fn run(config: TestConfig) {
-    let tests = TESTS
-        .iter()
-        .filter(|(name, _)| match &config {
-            TestConfig::None => false,
-            TestConfig::Include(include) => include.iter().any(|s| s == *name),
-            TestConfig::Exclude(exclude) => !exclude.iter().any(|s| s == *name),
-            TestConfig::All => true,
-        })
-        .collect::<Vec<_>>();
+    let tests = match config {
+        TestConfig::None => {
+            log::info!("tests disabled");
+            return;
+        }
+        TestConfig::Include(include) => {
+            let mut tests = Vec::new();
+            for name in &include {
+                if let Some(test) = TESTS.iter().find(|(n, _)| n == name) {
+                    tests.push(test)
+                } else {
+                    panic!("unknown test {name:?}");
+                }
+            }
+            tests
+        }
+        TestConfig::Exclude(exclude) => {
+            let mut tests = TESTS.iter().collect::<Vec<_>>();
 
-    log::info!("running {} tests", tests.len());
+            for name in &exclude {
+                let Some(idx) = tests
+                    .iter()
+                    .enumerate()
+                    .find(|(_, (n, _))| n == name)
+                    .map(|(i, _)| i)
+                else {
+                    panic!("unknown test {name:?}");
+                };
+                tests.remove(idx);
+            }
+
+            tests
+        }
+        TestConfig::All => {
+            log::info!("running all {} tests", TESTS.len());
+            TESTS.iter().collect()
+        }
+    };
+
     for (name, test) in tests {
         log::trace!("running {name:?}");
         test();
